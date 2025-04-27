e30a384e2f560f5971f495112a321ebd990cd44b
jonslow123 <onslow.jack@yahoo.com>
2025-04-27 17:41:55 +0100

    Change Password text changes

--- utils/emailService.js
+++ utils/emailService.js
@@ -31,7 +31,7 @@ const sendVerificationEmail = async (user, token) => {
 };
 
 const sendPasswordResetEmail = async (user, token) => {
-  const resetUrl = `${process.env.API_BASE_URL}/api/auth/reset-password?token=${token}`;
+  const resetUrl = `${process.env.VERIFICATION_PAGE_URL}/reset-password?token=${token}`;
 
   const mailOptions = {
     from: process.env.EMAIL_FROM,
@@ -39,16 +39,14 @@ const sendPasswordResetEmail = async (user, token) => {
     subject: 'Reset Your Password - Voices App',
     text: `Hello ${user.firstName},\n\n
     You are receiving this email because you (or someone else) has requested to reset your password.\n\n
-    Please click on the following link to reset your password:\n
-    ${resetUrl}\n\n
+    Please click on the following link to reset your password: ${resetUrl}\n\n
     If you did not request this, please ignore this email and your password will remain unchanged.\n\n
     This link will expire in 24 hours.\n\n
     Thanks,\nThe Voices App Team`,
     html: `
       <p>Hello ${user.firstName},</p>
       <p>You are receiving this email because you (or someone else) has requested to reset your password.</p>
-      <p>Please click on the following link to reset your password:</p>
-      <p><a href="${resetUrl}">${resetUrl}</a></p>
+      <p>Please <a href="${resetUrl}">Change Password Here</a></p>
       <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
       <p>This link will expire in 24 hours.</p>
       <p>Thanks,<br/>The Voices App Team</p>
